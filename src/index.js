09362fd49f2a31e16d71f203881df39c762e81c7
BexCdz <beckypearce92@hotmail.com>
2025-04-05 21:00:45 +0200

    Added basic JavaScript and form handling, added typewriter plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
+function generateActivity(event) {
+  event.preventDefault();
 
+  new Typewriter("#activity", {
+    strings: [
+      "Eiffel Tower at Sunset - Go to the top for sweeping views, or stay below with a picnic and watch the lights twinkle every hour after dark.",
+    ],
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+let activityFormElement = document.querySelector("#activity-generator-form");
+activityFormElement.addEventListener("submit", generateActivity);
