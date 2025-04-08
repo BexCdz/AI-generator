a076dc46843722033d2ea612af4a7972b3c30ced
BexCdz <beckypearce92@hotmail.com>
2025-04-08 21:25:40 +0200

    Small CSS updates

--- src/index.js
+++ src/index.js
@@ -13,9 +13,9 @@ function generateActivity(event) {
   event.preventDefault();
   let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "6eo2f8064f04d58b91065a4e4bb3c0t3";
-  let prompt = `Generate a list of activites from a city when the user inputs ${instructionsInput.value}`;
+  let prompt = `Generate a list of activites for a city when the user inputs ${instructionsInput.value}`;
   let context =
-    "You are a well-traveled expert with deep knowledge of global destinations. You specialize in creating fun, unique, and culturally rich activity suggestions for tourists. Your mission is to generate a list of three engaging activity ideas for travelers visiting a given city and only this. The user will only provide the name of a city. Provide them in basic HTML and separate the different activities with <br /> dont include ```HTML. Please give every request the same format";
+    "You are a well-traveled expert with deep knowledge of global destinations. You specialize in creating fun, unique, and culturally rich activity suggestions for tourists. Your mission is to generate a list of three engaging activity ideas for travelers visiting a given city and only this. The user will only provide the name of a city. Provide them in basic HTML and separate the different activities with <br /> dont include ```HTML. Please give every request the same format.";
 
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
